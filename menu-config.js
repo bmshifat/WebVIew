40f6d8f475c7196227eb089861ab4d7a55d22904
BM Shifat <43984284+bmshifat@users.noreply.github.com>
2021-04-04 17:50:16 +0600

    Update menu-config.js

--- menu-config.js
+++ menu-config.js
@@ -20,4 +20,18 @@
         ]
     },
 
+    {
+        label: 'Edit',
+        submenu: [
+           
+                {role : 'reload'},
+				{role : 'cut'},
+				{role : 'copy'},
+				{role : 'paste'},
+				{role : 'redo'},
+
+
+        ]
+    },
+
 ]
